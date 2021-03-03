b575d1a13f09d598463e85175166ef64ff533cb4
Dan Smith <dansmith@redhat.com>
2021-03-03 08:00:00 -0800

    Add a check on startup for staging directory
    If we have import methods enabled, we must have a staging directory
    in order to complete those operations. This adds a startup check
    for the staging directory with a warning log message if it is not
    found.
    
    Change-Id: Iac31d7f15ec6783a59b5e314883fb58f88fe2fd1

--- glance/tests/unit/common/test_wsgi_app.py
+++ glance/tests/unit/common/test_wsgi_app.py
@@ -149,3 +149,27 @@ class TestWsgiAppInit(test_utils.BaseTestCase):
         mock_conf.return_value = []
         wsgi_app.init_app()
         mock_Timer.assert_not_called()
+
+    @mock.patch('glance.common.wsgi_app._get_config_files')
+    @mock.patch('glance.async_._THREADPOOL_MODEL', new=None)
+    @mock.patch('glance.common.config.load_paste_app', new=mock.MagicMock())
+    def test_staging_store_uri_assertion(self, mock_conf):
+        self.config(node_staging_uri='http://good.luck')
+        mock_conf.return_value = []
+        # Make sure a staging URI with a bad scheme will abort startup
+        self.assertRaises(exception.GlanceException, wsgi_app.init_app)
+
+    @mock.patch('glance.common.wsgi_app._get_config_files')
+    @mock.patch('glance.async_._THREADPOOL_MODEL', new=None)
+    @mock.patch('glance.common.config.load_paste_app', new=mock.MagicMock())
+    @mock.patch('os.path.exists')
+    def test_staging_store_path_check(self, mock_exists, mock_conf):
+        mock_exists.return_value = False
+        mock_conf.return_value = []
+        with mock.patch.object(wsgi_app, 'LOG') as mock_log:
+            wsgi_app.init_app()
+            # Make sure that a missing staging directory will log a warning.
+            mock_log.warning.assert_called_once_with(
+                'Import methods are enabled but staging directory '
+                '%(path)s does not exist; Imports will fail!',
+                {'path': '/tmp/staging/'})
